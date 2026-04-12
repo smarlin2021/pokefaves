74ebfd098d9f0774b23bd5efa11853f5d915e5bc
Sierra Marlin <85563753+smarlin2021@users.noreply.github.com>
2026-04-11 22:35:46 -0400

    Replaced search bar with a text input, styling to come.

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,11 +1,41 @@
-import SearchBar from "../components/SearchBar";
+import { useState } from "react";
+import FavoriteCard from "../components/FavoriteCard";
+import { Grid, TextField } from "@mui/material";
+import data from "../data.json";
 
-function SearchPage() {
+const SearchPage = () => {
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const filteredCards = data.cards.filter((card) => {
+    if (query === "") return card;
+    return card.name.toLowerCase().includes(query.toLowerCase());
+  });
+
   return (
-    <div display="flex" justifyContent="center" alignItems="center">
-      <SearchBar />
+    <div display="flex" justifyContent="center">
+      <TextField
+        fullWidth
+        label="Search"
+        variant="outlined"
+        onChange={handleSearch}
+      />
+      <Grid item xs={12} sm={6} md={4} lg={3} key={data.cards.localId}>
+        {filteredCards.map((card) => (
+          <FavoriteCard
+            name={card.name}
+            id={card.id}
+            localId={card.localId}
+            image={card.image}
+            link={card.link}
+          />
+        ))}
+      </Grid>
     </div>
   );
-}
+};
 
 export default SearchPage;
